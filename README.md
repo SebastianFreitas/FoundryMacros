5b82b4ecc6ee42a12cd63d8b1727e7867e8839d1
ElTraffy <traffydeal@gmail.com>
2022-12-22 12:41:30 +0000

    Fixed xdx parser

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # FoundryMacros
  Js macros for foundry vtt 5e
+
+To use these macros, you need to toggle the macro panel from Chat to Script, paste the contents of a single js file into it, and its ready to use!
--- SingleAttack.js
+++ SingleAttack.js
@@ -1,9 +1,9 @@
 main()
 
-async function main(){
+async function main() {
   // Get Selected
   let selected = canvas.tokens.controlled;
-  if(selected.length > 1){
+  if (selected.length > 1) {
     ui.notifications.error("Please select only one token")
     return;
   }
@@ -11,7 +11,7 @@ async function main(){
   let selected_actor = selected[0].actor;
   // Get Target
   let targets = Array.from(game.user.targets)
-  if(targets.length == 0 || targets.length > 1 ){
+  if (targets.length == 0 || targets.length > 1) {
     ui.notifications.error("Please target one token");
     return;
   }
@@ -24,7 +24,7 @@ async function main(){
   actorWeapons.sort((a, b) => (a.system.attackBonus < b.system.attackBonus) ? 1 : -1)
 
   let weaponOptions = ""
-  for(let item of actorWeapons){
+  for (let item of actorWeapons) {
     weaponOptions += `<option value=${item.id}>${item.data.name} | ATK: ${item.system.attackBonus}</option>`
   }
 
@@ -39,144 +39,155 @@ async function main(){
     </div>
   `
   new Dialog({
-    title: "Roll Attack", 
+    title: "Roll Attack",
     content: dialogTemplate,
     buttons: {
       rollAtk: {
-        label: "Roll Attack", 
+        label: "Roll Attack",
         callback: async (html) => {
-            let wepID = html.find("#weapon")[0].value;
-            let wep = selected_actor.items.find(item => item.id == wepID)
-            console.log(wep);
-            let modifier = html.find("#mod")[0].value;
-            let ignoreArmor = html.find("#ignoreArmor")[0].checked;
-            let advantage = html.find("#advantage")
-            // Roll Attack
-            let critTreshold = 18;
-            let isCrit = false;
-            let baseTohit = rollDie(1,20);
-            if(advantage > 0) baseTohit = Math.max(rollDie(1,20),rollDie(1,20))
-            else baseTohit = Math.min(rollDie(1,20),rollDie(1,20))
-            console.log("baseTohit "+ baseTohit)
-            isCrit = (baseTohit >= critTreshold) 
-            
-           
-            // See if Attack is Greater than their armor, if so
-            let result = parseInt(baseTohit)  +parseInt(wep.system.attackBonus ) +parseInt(modifier)  +parseInt(selected_actor.system.abilities.str.value) 
+          let wepID = html.find("#weapon")[0].value;
+          let wep = selected_actor.items.find(item => item.id == wepID)
+          console.log(wep);
+          let modifier = html.find("#mod")[0].value;
+          let ignoreArmor = html.find("#ignoreArmor")[0].checked;
+          let advantage = html.find("#advantage")
+          // Roll Attack
+          let critTreshold = 18;
+          let isCrit = false;
+          let baseTohit = rollDie(1, 20);
+          if (advantage > 0) baseTohit = Math.max(rollDie(1, 20), rollDie(1, 20))
+          else baseTohit = Math.min(rollDie(1, 20), rollDie(1, 20))
+          console.log("baseTohit " + baseTohit)
+          isCrit = (baseTohit >= critTreshold)
 
-            console.log("To hit "+ result)
 
-            // Print Chat with Button to Roll Damage
-            let chatTemplate = ""
-          
-            let armor = parseInt(target_actor.system.attributes.ac.value) && !ignoreArmor ? parseInt(target_actor.system.attributes.ac.value) : 0;
-            
-            if(result > armor){
-              chatTemplate = `
-              <p> Rolled: ${result} against ${armor} Target Armor </p>
-              <p> It was a Hit! </p>
-              <p> <button id="rollDamage">Roll Damage</button></p>
-              `
-            } else {
+          // See if Attack is Greater than their armor, if so
+          let result = parseInt(baseTohit) + parseInt(wep.system.attackBonus) + parseInt(modifier) + parseInt(selected_actor.system.abilities.str.value)
+
+          console.log("To hit " + result)
+
+          // Print Chat with Button to Roll Damage
+          let chatTemplate = ""
+
+          let armor = parseInt(target_actor.system.attributes.ac.value) && !ignoreArmor ? parseInt(target_actor.system.attributes.ac.value) : 0;
+          if (isCrit) {
             chatTemplate = `
-              <p> Rolled: ${result} against ${armor} Target Armor </p>
-              <p> It was a Miss! </p>
-              `          
+            <p> Rolled: ${result} against ${armor} Target Armor </p>
+            <p> It was a CRIT! </p>
+            <p> It was a CRIIIIIIIIIIIIT! </p>
+            <p> <button id="rollDamage">Roll Damage</button></p>
+            `
+          }
+          else {
+            if (result > armor) {
+              chatTemplate = `
+                <p> Rolled: ${result} against ${armor} Target Armor </p>
+                <p> It was a Hit! </p>
+                <p> <button id="rollDamage">Roll Damage</button></p>
+                `
+            } else {
+              chatTemplate = `
+                <p> Rolled: ${result} against ${armor} Target Armor </p>
+                <p> It was a Miss! </p>
+                `
             }
-            
-            ChatMessage.create({
+          }
+
+          ChatMessage.create({
             speaker: {
-                alias: selected_actor.name
+              alias: selected_actor.name
             },
             content: chatTemplate,
-            roll: roll
-            })
-            
-            // Roll Damage
-            Hooks.once('renderChatMessage', (chatItem, html) => {
+            result: result
+          })
+
+          // Roll Damage
+          Hooks.once('renderChatMessage', (chatItem, html) => {
             html.find("#rollDamage").click(() => {
-              
-                let finaldmg = 0;
-                let wepDmg = (wep.system.damage?.parts ? wep.system.damage.parts : "" )
 
-                for(let i = 0; i < wepDmg.length;i++){
-                    
-                    let baseFormula = wepDmg[i][0]
+              let finaldmg = selected_actor.system.abilities.str.value;
+              let wepDmg = (wep.system.damage?.parts ? wep.system.damage.parts : "")
 
-                    console.log(baseFormula+ " <- BASE ");
+              for (let i = 0; i < wepDmg.length; i++) {
+
+                let baseFormula = wepDmg[i][0]
+
+                console.log(baseFormula + " <- BASE ");
+
+                let regex = /([a-z]{4,})/;
+                let type = baseFormula.match(regex)
+                if (type == null) type = 'piercing'
+                else type = type[0]
+
+                console.log(type + "   type")
+
+                 regex = /([0-9]+)d([0-9]+)|[0-9]/g;
+                let listResult = baseFormula.match(regex);
+                console.log(listResult + "   RESULT")
+
+
+
                 
-                    
-                    let regex = /([0-9]+)d([0-9]+)|[0-9]/g;
-                    let listResult = baseFormula.match(regex);
-                    console.log(listResult+ "   RESULT")
 
+                let currentDamage = 0;
 
-                    regex = /([a-z]{4,})/;
-                    let type = baseFormula.match(regex)
-                    if(type == null) type = 'piercing'
-                    else type = type[0]
-                    
-                    console.log(type[0]+ "   type")
+                for (let i = 0; i < listResult.length; i++) {
 
-                    let currentDamage = 0;
+                  let currentRoll = 0;
+                  if (listResult[i].match(/([0-9]+)d([0-9]+)/) != null) {
 
-                    for(let i = 0; i < listResult.length; i++){
-                      console.log("errrrrrr")
-        
-                      let currentRoll = 0;
-                      if(listResult[i].match(/([0-9]+)d([0-9]+)/) != null){
-                          
-                        let x = parseInt(listResult[i][0])
-                        let xplit = listResult[i].split('d')
+                    let x = parseInt(listResult[i][0])
+                    console.log(listResult[i])
+                    let xplit = listResult[i].split('d')
 
-                        for(let a = 0; a < x; a++){
-                          currentRoll+= rollDie(1, parseInt(xplit[1]));
-                          console.log(currentRoll+" currentroll")
-                        }
-
-                        if(isCrit) currentRoll*=2;
-
-                      } else if(listResult[i].match(/([0-9]+)/) != null) currentRoll = parseInt(listResult[i])
-                        
-                        currentDamage += currentRoll;
-                        console.log(currentDamage+" litle bits boooois")
+                    for (let a = 0; a < parseInt(xplit[0]); a++) {
+                      currentRoll += rollDie(1, parseInt(xplit[1]));
                     }
 
-                    let resistances = target_actor.system.traits.dr.value
-                    let immunities = target_actor.system.traits.di.value
+                    if (isCrit) currentRoll *= 2;
 
+                  } else if (listResult[i].match(/([0-9]+)/) != null) currentRoll = parseInt(listResult[i])
 
-                    if(immunities.length > 0){
-                        if(searchStringInArray(type,immunities)!= -1) currentDamage=0;
-                        
-                    } 
-                    
-                    if(resistances.length > 0){
-                        if(searchStringInArray(type,resistances)!= -1) currentDamage*=.5;                       
-                    }
-
-                    finaldmg += currentDamage 
-
-
+                  currentDamage += currentRoll;
+                  console.log(currentDamage + " CURRENT DAMAGE")
                 }
 
-                let hp = target_actor.system.attributes.hp.value
-                let maxHp = target_actor.system.attributes.hp.max
-        
-                let updatedHp = finaldmg > hp ? 0 : hp - finaldmg
-        
-                target_actor.update({'system.attributes.hp.value': updatedHp > maxHp ? maxHp : updatedHp})
+                let resistances = target_actor.system.traits.dr.value
+                let immunities = target_actor.system.traits.di.value
 
-                ChatMessage.create({
+
+                if (immunities.length > 0 && searchStringInArray(type, immunities) != -1) {
+                   currentDamage = 0;
+                   console.log("is immune to"+ type);
+
+                } else
+                if (resistances.length > 0 && searchStringInArray(type, resistances) != -1) {
+                  currentDamage *= .5;
+                  console.log("is resistant to"+ type);
+                }
+
+                finaldmg += currentDamage 
+
+
+              }
+
+              let hp = target_actor.system.attributes.hp.value
+              let maxHp = target_actor.system.attributes.hp.max
+
+              let updatedHp = finaldmg > hp ? 0 : hp - finaldmg
+
+              target_actor.update({ 'system.attributes.hp.value': updatedHp > maxHp ? maxHp : updatedHp })
+
+              ChatMessage.create({
                 content: `You dealt ${finaldmg} damage!`
-                
-                });
-                
+
+              });
+
             })
-            })         
+          })
         }
-        }, 
-        close: {
+      },
+      close: {
         label: "Close"
       }
     }
@@ -184,11 +195,11 @@ async function main(){
 }
 
 
-function searchStringInArray (str, strArray) {
-    for (var j=0; j<strArray.length; j++) {
-        if (strArray[j].match(str)) return j;
-    }
-    return -1;
+function searchStringInArray(str, strArray) {
+  for (var j = 0; j < strArray.length; j++) {
+    if (strArray[j].match(str)) return j;
+  }
+  return -1;
 }
 
 function rollDie(min, max) {
